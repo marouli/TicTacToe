77afd40cfd8deb37adbf383b8fd6f308a2d76dcc
Maria Sidiropoulou <maroulisid@gmail.com>
2017-06-21 14:31:55 +0200

    Initial board implementation

--- game.js
+++ game.js
@@ -6,4 +6,40 @@ class Player {
 }
 
 
+class Board {
+    constructor(size) {
+        this._size = this._checkValidBoardSize(size);
+        this._state = this._makeInitialState();
+        this._turnCounter = 0;
+        this._possibleMoves = size * size;
+        this._possibleWinningMove = size * 2 - 1;
+    }
+
+    _checkValidBoardSize(size) {
+        // TODO implement method
+        return size;
+    }
+
+    _makeInitialState() {
+        // TODO implement method
+        return [[null, null, null], [null, null, null], [null, null, null]];
+    }
+
+    _checkValidCoordinates(row, column) {
+        // TODO implement method
+        return true;
+    }    
+
+    _checkForWin() {
+        // TODO implement method
+        return null; // returns winning player
+    }
+
+    makePlayerMove(player, row, column) {
+        // TODO implement method
+        return true; 
+    }
+
+}
+
 
